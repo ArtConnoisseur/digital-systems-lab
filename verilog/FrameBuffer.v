82a5aa68ffe91c14ad887405ed653188f1c769f2
Rudraksh Srivastava <rudraksh2805@gmail.com>
2026-03-25 12:50:05 +0000

    Frame Buffer reset bug fixed

--- verilog/FrameBuffer.v
+++ verilog/FrameBuffer.v
@@ -89,6 +89,11 @@ module FrameBuffer(
     initial
         $readmemb("Frame_Buffer_Init.mem", frame);
 
+    // Handle reset 
+    always @(posedge CLK) begin
+        if (RESET)
+            $readmemb("Frame_Buffer_Init.mem", frame);
+    end
     // The code block below implements the frame buffer logic for port A
 
     always @(posedge A_CLK) begin
